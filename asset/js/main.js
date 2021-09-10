87c75ecf1651ffa0746dcd20f5945cd926184b65
hoang15102000 <>
2021-09-10 22:47:39 +0700

    update full code

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,48 +1,159 @@
 
-window.addEventListener('load', function(){
-    generate()
-})
+window.onload = function onLoad() {
+  createGrid(2, 4,'#container1');
+  createGrid(2, 4,'#container2');
+  var firebaseConfig = {
+    apiKey: "AIzaSyAULD_5XOVsrub8anXNRIQkPdOEX8Z1Qjw",
+    authDomain: "project2-b7d1e.firebaseapp.com",
+    databaseURL: "https://project2-b7d1e-default-rtdb.firebaseio.com",
+    projectId: "project2-b7d1e",
+    storageBucket: "project2-b7d1e.appspot.com",
+    messagingSenderId: "732028616880",
+    appId: "1:732028616880:web:b826bd73c949e95b56fb75",
+    measurementId: "G-VMMDLQ5B39"
+    };
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+  var position = database.ref('Position/robot');
+  position.on('value', function(snapshot) {
+    var childData = snapshot.val();
+    pos=childData;
+    var x= pos.slice(0,1);
+    var y= pos.slice(2,3);
+    console.log(pos);
+    console.log(x);
+    console.log(y);
+    var cc= "#sq-"+x+y;
+    console.log(cc);
+    changeSquareColor(cc, 3);
+  });
+  const button = document.getElementById('button')
+    button.addEventListener('click', () => {
+    start();
+  })
+// $(document).ready(function() {
 
-function generate(){
-    let right = document.querySelector('.right_content')
-    let strRight = ``
-    let count = "11"
-    for(let i = 1; i <= 4; i++ ){
-        let row = `<div class="row">`
-        for(let j = 1; j <= 5; j++){
-            count = i.toString() + j
-            if(j == 1){
-                row += `<div class="item black" position='${count}'"></div>`
-            }else{
-                row += `<div class="item red" position='${count}'"></div>`
-            }
+// });
+//   $('.square').on('mouseenter', function(event) {
+//     var square = event.target;
+//     var colorId = $(square).data('color');
+
+//     if (colorId || colorId === 0) {
+//       var color = colors[colorId];
+    
+//       $(square).html('<span>'+color+'</span>');
+//     }
+//   });
+
+//   $('.square').on('mouseleave', function(event) {
+//     var square = event.target;
+  
+//     $(square).html('');
+//   });
+
+//   $('.square').on('click', function(event) {
+//     var square = event.currentTarget;
+//     var colorId = $(square).data('color');
+
+//     if (colorId === numColors) {
+//       changeSquareColor(square, 0);
+//     } else {
+//       var newColorId = colorId+1;
+    
+//       changeSquareColor(square, newColorId);
+//     }
+//   });
+};
+const colors = [
+  'black', 
+  'blue',
+  'cyan', 
+  'green', 
+  'magenta', 
+  'red', 
+  'yellow',
+  'orange'
+];
+const numColors = colors.length-1;
+function createGrid(numRows, numSquares,id) {
+  for (var i=0; i<numRows; i++) {
+    if(id=='#container1'){
+        $(id).append("<ul id='row-"+i+1+"' class='row'>");
+    }
+    else{
+        $(id).append("<ul id='row-"+i+2+"' class='row'>");
+    }
+
+    if (i===(numRows-1)) {
+      createSquares(numRows, numSquares,id);
+    };
+  }
+}
+function createSquares(numRows, numSquares,id2) {
+  var count = 0;
+  for (var i=0; i<numRows; i++) {
+    if(id2=='#container1'){
+        var $row = $('#row-'+i+1);
+    }
+    else{
+        var $row = $('#row-'+i+2);
+    }
+    for (var j=0; j<numSquares; j++) {
+        if(id2=='#container1'){
+            var square = "<li id='sq-"+count+1+"' class='square'></li>";
+            $row.append(square);
         }
-        row += `</div>`
-
-        strRight += row
+        else{
+            var square = "<li id='sq-"+count+2+"' class='square'></li>";
+            $row.append(square);
+        }
+        // var square = "<li id='sq-"+count+"' class='square'></li>";
+        // $row.append(square);
+        count++;
+        if (j===(numSquares-1)) {
+            colorSquares(count,id2);
+        }
     }
-
-    right.innerHTML = strRight;
+  }
 }
-
-function setColor(position){
-   
-    if(position == 100){
-        generate()
-    }else{
-        document.querySelector(`div [position='${position}']`).style.backgroundColor = "green"
+function colorSquares(numSquares,id2) {
+  var colorId = 0;
+  
+  for (var i=0; i<numSquares; i++) {
+    //var square = '#sq-'+i;
+    if(id2=='#container1'){
+        var square = '#sq-'+i+1;
     }
-
+    else{
+        var square = '#sq-'+i+2;
+    }
+    colorId=5;
+    changeSquareColor(square, colorId);
+    
+    
+    // if (colorId < numColors) {
+    //   changeSquareColor(square, colorId);
+    //   colorId++;
+    // } else {
+    //   changeSquareColor(square, colorId);
+    //   colorId = 0;
+    // }
+  }
 }
-
-var start = document.querySelector(".start")
-
-start.addEventListener('click', function(){
-
-    let y = document.querySelector('.input_x')
-    let x = document.querySelector('.input_y')
-
-    let value = parseInt( x.value + y.value )
-    setColor(value)
-})
-
+function changeSquareColor(square, newColorId) {
+  var newColor = colors[newColorId];
+  
+  $(square).data('color', newColorId);
+  $(square).html('<span>'+newColor+'</span>');
+  $(square).css('background-color', newColor);
+};
+function start()  {
+  var x = document.getElementById("input_x").value; // A String value
+  var y = document.getElementById("input_y").value; // A String value
+  var c=x+0+y;
+  console.log(x);
+  console.log(y);
+  console.log(c);
+  var database = firebase.database();
+  database.ref('Position/user').set(c);
+};
