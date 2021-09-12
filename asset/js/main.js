2be0f48a700e28b4a2cd3dbc7f07a928a7b5461e
hoang15102000 <>
2021-09-12 11:26:06 +0700

    update login code
    code login

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,5 @@
 window.onload = function onLoad() {
+<<<<<<< Updated upstream
   gen(6, 7);
   var firebaseConfig = {
     apiKey: "AIzaSyAULD_5XOVsrub8anXNRIQkPdOEX8Z1Qjw",
@@ -11,6 +12,10 @@ window.onload = function onLoad() {
     measurementId: "G-VMMDLQ5B39"
   };
   firebase.initializeApp(firebaseConfig);
+=======
+  //tao grid
+  gen(6,7);
+>>>>>>> Stashed changes
   var database = firebase.database();
   var position = database.ref('Position/robot');
   position.on('value', function (snapshot) {
@@ -38,11 +43,21 @@ window.onload = function onLoad() {
     
 
   });
+  //nut nhan start
   const button = document.getElementById('button')
   button.addEventListener('click', () => {
     start();
+  });
+  // Kiem tra nguoi dung hien tai
+  firebase.auth().onAuthStateChanged((user)=>{
+    if(!user){
+        location.replace("index.html")
+    }else{
+        document.getElementById("user").innerHTML = "Hello, "+user.email
+    }
   })
 };
+<<<<<<< Updated upstream
 const colors = [
   'black',
   'blue',
@@ -78,6 +93,73 @@ function changeSquareColor(square, newColorId) {
   $(square).css('background-color', newColor);
 };
 function start() {
+=======
+//ham tao grid
+function gen(numrow, numcol){
+  for(let i = 1; i <= numrow; i++ ){
+    $('#container1').append("<ul id='row-"+i+"' class='row'>");
+    var $row = $('#row-'+i);
+      for(let j = 1; j <= numcol; j++){
+        count = i.toString() + j
+        var square = "<li id='sq-"+count+"' class='square'> </li>";
+        $row.append(square);
+        document.querySelector('#sq-'+count).style.backgroundColor = "gray"
+    }
+  }
+}
+//ham setcolor tai vi tri x y
+function setColor(x,y){
+    var database = firebase.database();
+    var old = database.ref('Robot/old');
+    old.on('value', function(snapshot) {
+      var childData = snapshot.val();   
+      xy_old=childData;
+      x_old= xy_old.slice(0,1);
+      y_old= xy_old.slice(2,3);
+    });
+    if(x==x_old&&y>y_old){
+      var j =(parseInt(y)-1).toString();
+      document.querySelector('#sq-'+x+j).style.backgroundColor = "gray"
+      document.querySelector('#sq-'+x+y).style.backgroundColor = "green"
+      document.querySelector('#sq-'+x+y).style.border="0.5em solid gray";
+      xy_old=(x+0+y).toString();
+      var database = firebase.database();
+      database.ref('Robot/old').set(xy_old);
+    }
+    if(x==x_old&&y<y_old){
+      var j =(parseInt(y)+1).toString();
+      document.querySelector('#sq-'+x+j).style.backgroundColor = "gray"
+      document.querySelector('#sq-'+x+y).style.backgroundColor = "green"
+      document.querySelector('#sq-'+x+y).style.border="0.5em solid gray";
+      xy_old=(x+0+y).toString();
+      var database = firebase.database();
+      database.ref('Robot/old').set(xy_old);
+    }
+    if(y==y_old&&x>x_old){
+      var j =(parseInt(x)-1).toString();
+      document.querySelector('#sq-'+j+y).style.backgroundColor = "gray"
+      document.querySelector('#sq-'+x+y).style.backgroundColor = "green"
+      document.querySelector('#sq-'+x+y).style.border="0.5em solid gray";
+      xy_old=(x+0+y).toString();
+      var database = firebase.database();
+      database.ref('Robot/old').set(xy_old);
+    }
+    if(y==y_old&&x<x_old){
+      var j =(parseInt(x)+1).toString();
+      document.querySelector('#sq-'+j+y).style.backgroundColor = "gray"
+      document.querySelector('#sq-'+x+y).style.backgroundColor = "green"
+      document.querySelector('#sq-'+x+y).style.border="0.5em solid gray";
+      xy_old=(x+0+y).toString();
+      var database = firebase.database();
+      database.ref('Robot/old').set(xy_old);
+    }
+    if(y==y_old&&x==x_old){
+      document.querySelector('#sq-'+x+y).style.backgroundColor = "green"
+    }
+}
+//ham gui data user len firebase
+function start()  {
+>>>>>>> Stashed changes
   var x = document.getElementById("input_x").value; // A String value
   var y = document.getElementById("input_y").value; // A String value
   var c = x + 0 + y;
@@ -86,4 +168,8 @@ function start() {
   console.log(c);
   var database = firebase.database();
   database.ref('Position/user').set(c);
+};
+//ham logout
+function logout(){
+  firebase.auth().signOut()
 };
