3cc81252738bbb20c0ae9fd249278a50917c5257
dokun1812 <dokun18122000@gmail.com>
2021-09-15 18:49:52 +0700

    Them chu thich, chinh sua giao dien, them nut home

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,8 +1,8 @@
-var x_old=0;
-var y_old=0;
-var user_x_old=0;
-var user_y_old=0;
-var cc=0;
+var x_old = 0;
+var y_old = 0;
+var user_x_old = 0;
+var user_y_old = 0;
+var cc = 0;
 window.onload = function onLoad() {
   //tao grid
   gen(6, 7);
@@ -13,30 +13,41 @@ window.onload = function onLoad() {
     pos = childData;
     var x = pos.slice(0, 1);
     var y = pos.slice(2, 3);
-    setColor(x,y);
-    console.log("pos+"+pos);
+    setColor(x, y);
+    console.log("pos+" + pos);
   });
 
   var usr = database.ref('User/user');
-  usr.on('value', function(snapshot) {
-    var childData = snapshot.val();   
-    user=childData;
-    var xx = user.slice(0, 1); 
+  usr.on('value', function (snapshot) {
+    var childData = snapshot.val();
+    user = childData;
+    var xx = user.slice(0, 1);
     var yy = user.slice(2, 3);
-    userdata(xx,yy,user);
+    userdata(xx, yy, user);
   });
 
   //nut nhan start
-
   const button = document.getElementById('button')
   button.addEventListener('click', () => {
-    //window.location.reload(true);
+    var database = firebase.database();
+    database.ref('Button/start').set(1);
     clear_user_old();
     start();
     userdata();
   });
-  //
-  
+  //nut nhan Stop
+  const button_stop = document.getElementById('button-stop')
+  button_stop.addEventListener('click', () => {
+    var database = firebase.database();
+    database.ref('Button/stop').set(1);
+  });
+  //nut nhan home
+  const button_home = document.getElementById('button-home')
+  button_home.addEventListener('click', () => {
+    var database = firebase.database();
+    database.ref('Button/home').set(1);
+  });
+
   // Kiem tra nguoi dung hien tai
   firebase.auth().onAuthStateChanged((user) => {
     if (!user) {
@@ -47,20 +58,23 @@ window.onload = function onLoad() {
   })
 };
 //ham tao grid
-function gen(numrow, numcol){
-  for(let i = 1; i <= numrow; i++ ){
-    $('#container1').append("<ul id='row-"+i+"' class='row'>");
-    var $row = $('#row-'+i);
-      for(let j = 1; j <= numcol; j++){
-        count = i.toString() + j
-        var square = "<li id='sq-"+count+"' class='square'> </li>";
-        $row.append(square);
-        // dieu khien gen ra cac o ke hang
-        if (i >= 2 && i <= 5 && j >= 2 && j <= 3 || i >= 2 && i <= 5 && j >= 5 && j <= 6) { 
-          document.querySelector('#sq-'+count).style.backgroundColor = "red"
-        } else {
-          document.querySelector('#sq-'+count).style.backgroundColor = "gray"
-        }
+function gen(numrow, numcol) {
+  for (let i = 1; i <= numrow; i++) {
+    $('#container1').append("<ul id='row-" + i + "' class='row'>");
+    var $row = $('#row-' + i);
+    for (let j = 1; j <= numcol; j++) {
+      count = i.toString() + j
+      var square = "<li id='sq-" + count + "' class='square'> </li>";
+      $row.append(square);
+      // dieu khien gen ra cac o ke hang
+
+      if (i >= 2 && i <= 5 && j >= 2 && j <= 3 || i >= 2 && i <= 5 && j >= 5 && j <= 6) {
+        document.querySelector('#sq-' + count).style.backgroundColor = "red"
+      } else if (i == 1 && j == 1) {
+        document.querySelector('#sq-' + count).style.backgroundColor = "black"
+      } else {
+        document.querySelector('#sq-' + count).style.backgroundColor = "gray"
+      }
     }
   }
 }
@@ -81,45 +95,86 @@ function setColor(x, y) {
     if (x >= 2 && x <= 5 && j >= 2 && j <= 3 || x >= 2 && x <= 5 && j >= 5 && j <= 6) {
       document.querySelector('#sq-' + x + j).style.backgroundColor = "red"
       document.querySelector('#sq-' + x + j).style.border = "0em solid red";
+    } else if (x == 1 && j == 1) {
+      document.querySelector('#sq-' + x + j).style.backgroundColor = "black"
+      document.querySelector('#sq-' + x + j).style.border = "0em solid black";
     } else {
       document.querySelector('#sq-' + x + j).style.backgroundColor = "gray"
+      document.querySelector('#sq-' + x + j).style.border = "0em solid gray";
     }
 
     if (x >= 2 && x <= 5 && y >= 2 && y <= 3 || x >= 2 && x <= 5 && y >= 5 && y <= 6) {
       document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
       document.querySelector('#sq-' + x + y).style.border = "0.5em solid red";
+    } else if (x == 1 && y == 1) {
+      document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
+      document.querySelector('#sq-' + x + y).style.border = "0.5em solid black";
     } else {
       document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
       document.querySelector('#sq-' + x + y).style.border = "0.5em solid gray";
     }
-    xy_old = (x + 0 + y).toString();
+    x_y_old = (x + 0 + y).toString();
     var database = firebase.database();
-    database.ref('Robot/old').set(xy_old);
+    database.ref('Robot/old').set(x_y_old);
   }
+
   if (x == x_old && y < y_old) {
     var j = (parseInt(y) + 1).toString();
     if (x >= 2 && x <= 5 && j >= 2 && j <= 3 || x >= 2 && x <= 5 && j >= 5 && j <= 6) {
       document.querySelector('#sq-' + x + j).style.backgroundColor = "red"
       document.querySelector('#sq-' + x + j).style.border = "0em solid red";
+    } else if (x == 1 && j == 1) {
+      document.querySelector('#sq-' + x + j).style.backgroundColor = "black"
     } else {
       document.querySelector('#sq-' + x + j).style.backgroundColor = "gray"
     }
     if (x >= 2 && x <= 5 && y >= 2 && y <= 3 || x >= 2 && x <= 5 && y >= 5 && y <= 6) {
       document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
       document.querySelector('#sq-' + x + y).style.border = "0.5em solid red";
+    } else if (x == 1 && y == 1) {
+      document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
+      document.querySelector('#sq-' + x + y).style.border = "0.5em solid black";
     } else {
       document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
       document.querySelector('#sq-' + x + y).style.border = "0.5em solid gray";
     }
-    xy_old = (x + 0 + y).toString();
+    x_y_old = (x + 0 + y).toString();
     var database = firebase.database();
-    database.ref('Robot/old').set(xy_old);
+    database.ref('Robot/old').set(x_y_old);
   }
+
   if (y == y_old && x > x_old) {
     var j = (parseInt(x) - 1).toString();
     if (j >= 2 && j <= 5 && y >= 2 && y <= 3 || j >= 2 && j <= 5 && y >= 5 && y <= 6) {
       document.querySelector('#sq-' + j + y).style.backgroundColor = "red"
       document.querySelector('#sq-' + j + y).style.border = "0em solid red";
+    } else if (j == 1 && y == 1) {
+      document.querySelector('#sq-' + j + y).style.backgroundColor = "black"
+    } else {
+      document.querySelector('#sq-' + j + y).style.backgroundColor = "gray"
+    }
+    if (x >= 2 && x <= 5 && y >= 2 && y <= 3 || x >= 2 && x <= 5 && y >= 5 && y <= 6) {
+      document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
+      document.querySelector('#sq-' + x + y).style.border = "0.5em solid red";
+    } else if (x == 1 && y == 1) {
+      document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
+      document.querySelector('#sq-' + x + y).style.border = "0.5em solid black";
+    } else {
+      document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
+      document.querySelector('#sq-' + x + y).style.border = "0.5em solid gray";
+    }
+    x_y_old = (x + 0 + y).toString();
+    var database = firebase.database();
+    database.ref('Robot/old').set(x_y_old);
+  }
+
+  if (y == y_old && x < x_old) {
+    var j = (parseInt(x) + 1).toString();
+    if (j >= 2 && j <= 5 && y >= 2 && y <= 3 || j >= 2 && j <= 5 && y >= 5 && y <= 6) {
+      document.querySelector('#sq-' + j + y).style.backgroundColor = "red"
+      document.querySelector('#sq-' + j + y).style.border = "0em solid red";
+    } else if (j == 1 && y == 1) {
+      document.querySelector('#sq-' + j + y).style.backgroundColor = "black"
     } else {
       document.querySelector('#sq-' + j + y).style.backgroundColor = "gray"
     }
@@ -127,48 +182,42 @@ function setColor(x, y) {
     if (x >= 2 && x <= 5 && y >= 2 && y <= 3 || x >= 2 && x <= 5 && y >= 5 && y <= 6) {
       document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
       document.querySelector('#sq-' + x + y).style.border = "0.5em solid red";
+    } else if (x == 1 && y == 1) {
+      document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
+      document.querySelector('#sq-' + x + y).style.border = "0.5em solid black";
     } else {
       document.querySelector('#sq-' + x + y).style.backgroundColor = "green"
       document.querySelector('#sq-' + x + y).style.border = "0.5em solid gray";
     }
-    xy_old = (x + 0 + y).toString();
+    x_y_old = (x + 0 + y).toString();
     var database = firebase.database();
-    database.ref('Robot/old').set(xy_old);
-  }
-  if (y == y_old && x < x_old) {
-    var j = (parseInt(x) + 1).toString();
-    if (j >= 2 && j <= 5 && y >= 2 && y <= 3 || j >= 2 && j <= 5 && y >= 5 && y <= 6) {
-      document.querySelector('#sq-' + j + y).style.backgroundColor = "red"
-      document.querySelector('#sq-' + j + y).style.border = "0em solid red";
-    } else {
-      document.querySelector('#sq-' + j + y).style.backgroundColor = "gray"
-    }
+    database.ref('Robot/old').set(x_y_old);
   }
 }
 //lay user data
-function userdata(x,y,ccc){
+function userdata(x, y, ccc) {
   var database = firebase.database();
   var user_old = database.ref('User/old');
-  user_old.on('value', function(snapshot) {
-    var childData = snapshot.val();   
-    xxy_old=childData;
-    cc=xxy_old.toString();
-    user_x_old= xxy_old.slice(0,1);
-    user_y_old= xxy_old.slice(2,3);
+  user_old.on('value', function (snapshot) {
+    var childData = snapshot.val();
+    xxy_old = childData;
+    cc = xxy_old.toString();
+    user_x_old = xxy_old.slice(0, 1);
+    user_y_old = xxy_old.slice(2, 3);
     console.log(user_x_old);
     console.log(user_y_old);
   });
-  if(ccc!=cc){
-    if (user_x_old >= 2 && user_x_old <= 5 && user_y_old >= 2 && user_y_old <= 3 || user_x_old >= 2 && user_x_old <= 5 && user_y_old >= 5 && user_y_old<= 6) { 
-      document.querySelector('#sq-'+x+y).style.border="0.5em solid yellow";
-      document.querySelector('#sq-'+user_x_old+user_y_old).style.border="0.5em";
-    }else{
-      console.log('#sq-'+user_x_old+user_y_old);
-      document.querySelector('#sq-'+x+y).style.border="0.5em solid yellow";
-      document.querySelector('#sq-'+user_x_old+user_y_old).style.border="0.5em";
-      
+  if (ccc != cc) {
+    if (user_x_old >= 2 && user_x_old <= 5 && user_y_old >= 2 && user_y_old <= 3 || user_x_old >= 2 && user_x_old <= 5 && user_y_old >= 5 && user_y_old <= 6) {
+      document.querySelector('#sq-' + x + y).style.border = "0.5em solid yellow";
+      document.querySelector('#sq-' + user_x_old + user_y_old).style.border = "0.5em";
+    } else {
+      console.log('#sq-' + user_x_old + user_y_old);
+      document.querySelector('#sq-' + x + y).style.border = "0.5em solid yellow";
+      document.querySelector('#sq-' + user_x_old + user_y_old).style.border = "0.5em";
+
     }
-    xxy_old=(x+0+y).toString();
+    xxy_old = (x + 0 + y).toString();
     var database = firebase.database();
     database.ref('User/old').set(xxy_old);
   }
